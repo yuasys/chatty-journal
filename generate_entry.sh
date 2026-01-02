dd5e4fd32b09b8ca2352b731157c058d04118402
Nob <52148183+yuasys-git01@users.noreply.github.com>
2026-01-02 15:36:32 +0900

    スクリプト追記

--- generate_entry.sh
+++ generate_entry.sh
@@ -9,9 +9,12 @@ TARGET_DIR="$YEAR/$MONTH"
 YESTERDAY_STR=$(date -v-1d "+%Y/%m/%Y-%m-%d.md")
 TOMORROW_STR=$(date -v+1d "+%Y/%m/%Y-%m-%d.md")
 
-echo TODAY = "$TODAY"
-echo YEAR = "$YEAR"
-echo MONTH = "$MONTH"
-echo TARGET_DIR = "$TARGET_DIR"
-echo YESTERDAY_STR = "$YESTERDAY_STR"
-echo TOMORROW_STR = "$TOMORROW_STR"
+# 変数の内容を表示(デバック用）
+echo "TODAY = \"$TODAY\""
+echo "YEAR = \"$YEAR\""
+echo "MONTH = \"$MONTH\""
+echo "TARGET_DIR = \"$TARGET_DIR\""
+echo "YESTERDAY_STR = \"$YESTERDAY_STR\""
+echo "TOMORROW_STR = \"$TOMORROW_STR\""
+
+# 2. 「前の記事」のURLを特定 (既存のmdファイルから最新のものを検索)
